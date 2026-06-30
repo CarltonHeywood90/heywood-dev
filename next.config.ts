382ddefceb23e55b320280f06a50f098f46664c1
carlton <cjwood1@gmail.com>
2026-06-30 08:43:14 -0600

    added config options to config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // Optional: Add this if your images fail to load
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
